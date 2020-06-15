9e76917c61bb5ba30c8eeb384c850bead409827b
Denver Brierley <denver.brierley@gmail.com>
2020-06-15 19:26:53 +0100

    add reverse array

--- sumfinder.py
+++ sumfinder.py
@@ -6,6 +6,8 @@ print ("The target number is: ", target_number)
 random_array = [random.randint(1,50) for _ in range(50)]
 
 sorted_array = sorted(random_array)
+reversed_array = sorted_array[::-1]
 
-print(random_array)
+print (random_array)
 print (sorted_array)
+print (reversed_array)
