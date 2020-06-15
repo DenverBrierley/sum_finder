e54195d263c33333e8788c4bdd7b0ed23f571098
Denver Brierley <denver.brierley@gmail.com>
2020-06-15 18:20:53 +0100

    created target and random array

---
+++ sumfinder.py
@@ -0,0 +1,8 @@
+import random
+
+target_number = random.randint(2,100)
+print ("The target number is: ", target_number)
+
+random_array = [random.randint(1,10) for _ in range(50)]
+
+print(random_array)
