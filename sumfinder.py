b91e4e5729b5a0c5f226ec9bf5704c3a339d9dc1
Denver Brierley <denver.brierley@gmail.com>
2020-06-15 18:24:20 +0100

    cahnged random number ranges, added a sorted array

--- sumfinder.py
+++ sumfinder.py
@@ -1,8 +1,11 @@
 import random
 
-target_number = random.randint(2,100)
+target_number = random.randint(50,100)
 print ("The target number is: ", target_number)
 
-random_array = [random.randint(1,10) for _ in range(50)]
+random_array = [random.randint(1,50) for _ in range(50)]
+
+sorted_array = sorted(random_array)
 
 print(random_array)
+print (sorted_array)
